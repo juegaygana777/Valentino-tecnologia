b8b3427608e4b96eaf83341c6ce0cdf0be98e6f9
juegaygana777 <valentinolocal@gmail.com>
2026-08-17 20:10:23 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const revalidate = 0;
-export default async function Home(const supabase = createClient(
+export default async function Home() {
+  const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
